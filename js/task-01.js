c6c8b158a83f269cd079c4b09837e001c49205d8
Y-Savushchyk <savushchky@gmail.com>
2023-02-01 17:35:21 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,11 @@
-const categoriesItemRef = ('#categories')
-categoriesItemRef.querySelectorAll('.item')
-    console.log(categoriesItemRef);
+/*step 1*/
+const categoriesListRef = document.querySelectorAll(".item");
+console.log(`Number of categories: ${categoriesListRef.length}`);
+
+/*step 2*/
+const categorieListRef = document.querySelectorAll("#categories ul");
+const categorieTitleRef = document.querySelectorAll("h2");
+
+for (let i = 0; i < categoriesListRef.length; i += 1) {
+  console.log(`Category: ${categorieTitleRef[i].textContent}`);
+  console.log(`Elements: ${categorieListRef[i].children.length}`);
