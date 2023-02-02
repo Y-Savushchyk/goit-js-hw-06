2656fd07b5da22c10beeb355fa5a5f6450f49b59
Y-Savushchyk <savushchky@gmail.com>
2023-02-02 19:14:21 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,19 +1,19 @@
 /*step 1*/
-const btn = document.querySelectorAll("#counter button")
-console.log(btn);
+const btn = document.querySelectorAll("#counter button");
+
 /*step 2*/
-const counterValue = document.querySelector("#value")
-counterValue.textContent=0
+const counterValue = document.querySelector("#value");
+counterValue.textContent = 0;
 /*step 3*/
 
 const decrementClick = () => {
-    // counterValue.textContent = counterValue.textContent - 1
-    counterValue.textContent=  counterValue.textContent-(-btn[0].textContent)
+  // counterValue.textContent = counterValue.textContent - 1
+  counterValue.textContent = counterValue.textContent - -btn[0].textContent;
 };
-btn[0].addEventListener("click", decrementClick)
+btn[0].addEventListener("click", decrementClick);
 const incrementClick = () => {
-    // counterValue.textContent=  counterValue.textContent-1+2
-    // counterValue.textContent = counterValue.textContent - (-1)
-    counterValue.textContent=  counterValue.textContent-(-btn[1].textContent)
+  // counterValue.textContent=  counterValue.textContent-1+2
+  // counterValue.textContent = counterValue.textContent - (-1)
+  counterValue.textContent = counterValue.textContent - -btn[1].textContent;
 };
-btn[1].addEventListener("click", incrementClick)
+btn[1].addEventListener("click", incrementClick);
