fa3b1e3ff16d9ccd5e3229d9b97669ce8d985c25
Y-Savushchyk <115530650+Y-Savushchyk@users.noreply.github.com>
2023-02-04 13:48:22 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,10 @@
+const buttonRef = document.querySelector("button")
+const bodyRef = document.querySelector("body")
+const spanRef = document.querySelector("span")
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+buttonRef.addEventListener("click", ()=>{
+  bodyRef.style.backgroundColor=getRandomHexColor()
+  spanRef.textContent=getRandomHexColor()
+})
