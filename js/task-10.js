58591d6a2578e2b35e418e83be5684401c1832d7
Y-Savushchyk <115530650+Y-Savushchyk@users.noreply.github.com>
2023-02-04 15:04:31 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,14 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+// const boxesDivRef = document.querySelector("#boxes")
+// const buttonCreateRef = document.querySelector("button[data-create]")
+// const buttonDestroyRef = document.querySelector("button[data-destroy")
+// const inputRef = document.querySelector("input")
+
+
+// buttonCreateRef.addEventListener("click", createBoxes)
+// function createBoxes() {
+ 
+// }
+
