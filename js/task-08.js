a3ea23b67c6af5f7a54ea29b0af38baf633abdbf
Y-Savushchyk <savushchky@gmail.com>
2023-02-04 19:55:20 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,14 +1,34 @@
-const formRef = document.querySelector(".login-form")
-formRef.addEventListener("submit", clickSubmit)
+/*
+const formRef = document.querySelector(".login-form");
+formRef.addEventListener("submit", clickSubmit);
 function clickSubmit(event) {
-    event.preventDefault();
-    const {
-        elements: { email, password } 
-     }= event.currentTarget;
-    if (email.value===""||password.value==="") {
-        alert ("Все поля должны быть заполнены")
-    } console.log(`email: ${email.value}, password: ${password.value}`);
-    formRef.reset()
-    
-    
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    return alert("Все поля должны быть заполнены");
+  }
+  console.log(`email: ${email.value}, password: ${password.value}`);
+  formRef.reset();
+}
+*/
+const formRef = document.querySelector(".login-form");
+formRef.addEventListener("submit", clickSubmit);
+const valuesRef = {
+  email: "email",
+  password: "password",
+};
+function clickSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    return alert("Все поля должны быть заполнены");
+  }
+  valuesRef.email = email.value;
+  valuesRef.password = password.value;
+  formRef.reset();
+  return console.log(valuesRef);
 }
