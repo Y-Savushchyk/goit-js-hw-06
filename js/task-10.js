2cd3dc8e30ceb6cbca40133c34a24a279a4d8ec9
Y-Savushchyk <savushchky@gmail.com>
2023-02-04 15:58:54 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,14 +1,23 @@
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-// }
-// const boxesDivRef = document.querySelector("#boxes")
-// const buttonCreateRef = document.querySelector("button[data-create]")
-// const buttonDestroyRef = document.querySelector("button[data-destroy")
-// const inputRef = document.querySelector("input")
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const boxesDivRef = document.querySelector("#boxes");
+boxesDivRef.style.display = "flex";
+const buttonCreateRef = document.querySelector("button[data-create]");
+const buttonDestroyRef = document.querySelector("button[data-destroy");
+const inputRef = document.querySelector("input");
 
-
-// buttonCreateRef.addEventListener("click", createBoxes)
-// function createBoxes() {
- 
-// }
-
+buttonCreateRef.addEventListener("click", createBoxes);
+function createBoxes() {
+  for (let i = 1; i <= inputRef.value; i += 1) {
+    const newDivRef = document.createElement("div");
+    newDivRef.style.backgroundColor = getRandomHexColor();
+    newDivRef.style.width = `${40}px`;
+    newDivRef.style.height = `${40}px`;
+    boxesDivRef.append(newDivRef);
+    boxesDivRef.firstChild.style.width = `${30}px`;
+    boxesDivRef.firstChild.style.height = `${30}px`;
+  }
+}
+buttonDestroyRef.addEventListener("click", destroyBoxes);
+function destroyBoxes() {}
