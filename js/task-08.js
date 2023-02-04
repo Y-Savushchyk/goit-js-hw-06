a5f1d47f6c4a0bb5462e861ddd9c7aca29d52362
Y-Savushchyk <115530650+Y-Savushchyk@users.noreply.github.com>
2023-02-04 13:01:44 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,14 @@
-
+const formRef = document.querySelector(".login-form")
+formRef.addEventListener("submit", clickSubmit)
+function clickSubmit(event) {
+    event.preventDefault();
+    const {
+        elements: { email, password } 
+     }= event.currentTarget;
+    if (email.value===""||password.value==="") {
+        alert ("Все поля должны быть заполнены")
+    } console.log(`email: ${email.value}, password: ${password.value}`);
+    formRef.reset()
+    
+    
+}
