06500adc91d9a8fc6871e7e6178d1278349c6a15
Y-Savushchyk <115530650+Y-Savushchyk@users.noreply.github.com>
2023-02-04 13:04:59 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -2,6 +2,6 @@ const inputRef = document.querySelector("#font-size-control");
 const textRef = document.querySelector("#text");
 inputRef.addEventListener("input", inputChange);
 function inputChange(event) {
-  console.log(event.currentTarget.value);
+  // console.log(event.currentTarget.value);
   textRef.style.fontSize = `${event.currentTarget.value}px`;
 }
